21ecb5f2ed924490125738de51b40d14ed8b1450
Md Firoz Ahmed <82923134+FirozAhmed01766762474@users.noreply.github.com>
2023-10-02 10:05:19 +0600

    Add files via upload

---
+++ eTickets/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace eTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MoviesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var movies = await _db.Movies.Include(n => n.Cinema).OrderBy(n => n.Name).ToListAsync();
+            return View(movies);
+        }
+    }
+}
